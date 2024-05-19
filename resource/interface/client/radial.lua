a36c6fbee131ba24de0b7e76986e65c2993cbd98
mur4i <murainobeat@gmail.com>
2024-05-19 19:13:36 -0300

    Update ox_lib redesign and readme

--- resource/interface/client/radial.lua
+++ resource/interface/client/radial.lua
@@ -339,7 +339,7 @@ lib.addKeybind({
             Wait(0)
         end
     end,
-    -- onReleased = lib.hideRadial,
+    onReleased = lib.hideRadial, --murai
 })
 
 AddEventHandler('onClientResourceStop', function(resource)
